85b5794ae3cf1fad44e9935570f75035978fc0b9
AJ <anthonyfields1@gmail.com>
2020-06-03 00:05:03 -0400

    Added basic generator for lists

--- dw/about/routes.py
+++ dw/about/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, url_for
 from dw.about import bp
+from dw.models import Name
 
 @bp.route('/diceware')
 def diceware():
-    return render_template('about/diceware.html')
+    curr_lists = Name.query.all()
+    return render_template('about/diceware.html', names=curr_lists)
 
 @bp.route('/project')
 def project():
@@ -12,3 +14,8 @@ def project():
 @bp.route('/author')
 def author():
     return render_template('about/author.html')
+
+@bp.route('lists/<listname>')
+def get_list(listname):
+    curr_lists = Name.query.filter(Name.name==listname).first()
+    return render_template('about/lists.html', name=curr_lists.name, rolls=curr_lists.words)
