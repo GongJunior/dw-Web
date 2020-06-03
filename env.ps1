60492a83d34049d0a7d84fb1dd8f85220d7b25a8
AJ <anthonyfields1@gmail.com>
2020-06-03 00:16:21 -0400

    text change

--- env.ps1
+++ env.ps1
@@ -1,4 +1,4 @@
 $env:FLASK_APP = "start.py"
-$env:FLASK_ENV = "development"
-#$env:FLASK_ENV = "production"
+#$env:FLASK_ENV = "development"
+$env:FLASK_ENV = "production"
 gci env: | where name -Like flask*
